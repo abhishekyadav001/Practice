302a1c20fd711f58bcd22bc5249291c08154dbe1
abhishekyadav <yadavabhishek2723@gmail.com>
2023-09-16 22:48:55 +0530

    new practice

--- css practice/New folder/index.js
+++ css practice/New folder/index.js
@@ -1,4 +1,12 @@
-let div = document.getElementsByClassName("nav-child");
-div.addEventListener("click", (e) => {
-  console.log(div.innerText);
+document.querySelector(".nav-cart").addEventListener("mouseenter", () => {
+  document.getElementById("navb-cart").style.display = "block";
 });
+
+document.querySelector(".nav-cart").addEventListener("mouseleave", () => {
+  document.getElementById("navb-cart").style.display = "none";
+});
+document.querySelector("#navb-cart").addEventListener("mouseleave", () => {
+  document.querySelector("#navb-cart").style.display = "none";
+  console.log("hell");
+});
+document.querySelector(".nav-page").addEventListener("mouseenter", () => {});
